76326313897c5f976bff109ca075633d329bf453
MicheleIanni86 <micheleianni86@gmail.com>
2024-03-26 16:53:40 +0100

    feat: Added Button for Return in comics.index

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -6,28 +6,34 @@
 
 <section>
     <div class="container">
-      <h1 class="my-5">Dettaglio Fumetto {{ $comic->id }}: {{ $comic->name }}</h1>
+
+        <a class="btn btn-primary my-4" href="{{ route('comics.index') }}"><i class="fa-solid fa-left-long"></i> Torna Indietro</a>
+      <h1 class="mb-5">Dettaglio Fumetto: <strong>{{ $comic->title }}</strong></h1>
     
     <div class="row">
-        <div class="col-5">
+        <div class="col-2">
             <img src="{{ $comic->thumb }}" class="img-fluid" alt="">
         </div>
-        <div class="col-7">
-            <h2 class="h5">Descrizione</h2>
+        <div class="col-10">
+            <h2 class="h3">Descrizione</h2>
             <p>{{ $comic->description }}</p>
 
-            <div class="row">
-                <div class="col-4">
+            <div class="row text-center mt-5">
+                <div class="col-3">
                     <h2 class="h5">Prezzo</h2>
-                    <p>{{ $comic->price }}</p>          
+                    <p class="fw-bold">{{ $comic->price }}</p>          
                 </div>
-                <div class="col-4">
+                <div class="col-3">
+                    <h2 class="h5">Serie</h2>
+                    <p class="fw-bold">{{ $comic->series }}</p>       
+                </div>
+                <div class="col-3">
                     <h2 class="h5">Data Uscita</h2>
-                    <p>{{ $comic->sale_date }}</p>       
+                    <p class="fw-bold">{{ $comic->sale_date }}</p>       
                 </div>
-                <div class="col-4">
+                <div class="col-3">
                     <h2 class="h5">Tipo</h2>
-                    <p>{{ $comic->type }}</p>       
+                    <p class="fw-bold">{{ $comic->type }}</p>       
                 </div>
             </div>
         </div>
@@ -36,4 +42,8 @@
     </div>
 </section>
 
+@endsection
+
+@section('css')
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 @endsection
